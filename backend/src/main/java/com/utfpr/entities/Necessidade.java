ca88e869a657d5c5c3fdddeff3af9294e659cdff
Gabriel Cosato <gabrielcosato@gmail.com>
2025-05-12 21:51:09 -0300

    init Necessidade

--- backend/src/main/java/com/utfpr/entities/Necessidade.java
+++ backend/src/main/java/com/utfpr/entities/Necessidade.java
@@ -1,4 +1,99 @@
 package com.utfpr.entities;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_necessidade")
 public class Necessidade {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Integer quantidadeNecessaria;
+    private Integer quantidadeRecebida;
+    private String unidadeMedida;
+
+    public Necessidade(){
+
+    }
+
+    public Necessidade(Long id, String nome, Integer quantidadeNecessaria, Integer quantidadeRecebida, String unidadeMedida) {
+        this.id = id;
+        this.nome = nome;
+        this.quantidadeNecessaria = quantidadeNecessaria;
+        this.quantidadeRecebida = quantidadeRecebida;
+        this.unidadeMedida = unidadeMedida;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getQuantidadeNecessaria() {
+        return quantidadeNecessaria;
+    }
+
+    public void setQuantidadeNecessaria(Integer quantidadeNecessaria) {
+        this.quantidadeNecessaria = quantidadeNecessaria;
+    }
+
+    public Integer getQuantidadeRecebida() {
+        return quantidadeRecebida;
+    }
+
+    public void setQuantidadeRecebida(Integer quantidadeRecebida) {
+        this.quantidadeRecebida = quantidadeRecebida;
+    }
+
+    public String getUnidadeMedida() {
+        return unidadeMedida;
+    }
+
+    public void setUnidadeMedida(String unidadeMedida) {
+        this.unidadeMedida = unidadeMedida;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Necessidade that)) return false;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public void atualizarQuantidadeRecebida (int quantidade){
+
+    }
 }
