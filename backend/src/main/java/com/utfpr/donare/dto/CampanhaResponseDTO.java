4a0a5e04b75b03491135c341b8cc51482a7c712e
Camila <camilazbregalda@gmail.com>
2025-06-29 21:37:29 -0300

    feat: Adicionando retorno personalizado de campanha no findById

--- backend/src/main/java/com/utfpr/donare/dto/CampanhaResponseDTO.java
+++ backend/src/main/java/com/utfpr/donare/dto/CampanhaResponseDTO.java
@@ -1,5 +1,6 @@
 package com.utfpr.donare.dto;
 
+import com.utfpr.donare.domain.Campanha;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,4 +25,29 @@ public class CampanhaResponseDTO {
     private LocalDateTime dt_fim;
     private String organizador;
     private List<PostagemResponseDTO> postagens = new ArrayList<>();
+    private List<UserResponseDTO> voluntarios = new ArrayList<>();
+    private List<UserResponseDTO> usuariosQueSeguem = new ArrayList<>();
+
+    public CampanhaResponseDTO(Campanha campanha) {
+        this.id = campanha.getId();
+        this.titulo = campanha.getTitulo();
+        this.descricao = campanha.getDescricao();
+        this.categoriaCampanha = campanha.getCategoriaCampanha();
+        this.endereco = endereco;
+        this.imagemCapa = null;
+        this.status = campanha.getStatus();
+        this.tipoCertificado = campanha.getTipoCertificado();
+        this.dtInicio = campanha.getDtInicio();
+        this.dt_fim = campanha.getDt_fim();
+        this.organizador = campanha.getOrganizador();
+        this.postagens = campanha.getPostagens().stream()
+                .map(PostagemResponseDTO::new)
+                .toList();
+        this.voluntarios = campanha.getVoluntarios().stream()
+                .map(UserResponseDTO::new)
+                .toList();
+        this.usuariosQueSeguem = campanha.getUsuariosQueSeguem().stream()
+                .map(UserResponseDTO::new)
+                .toList();
+    }
 }
